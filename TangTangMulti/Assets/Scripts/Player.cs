c7b96eb323d2634f832dea8952d51e7cfa25405c
Browniesss <abcdkl45678@gmail.com>
2023-09-19 21:22:03 +0900

    캐릭터 스크립트 및 애니메이터 업로드

--- TangTangMulti/Assets/Scripts/Player.cs
+++ TangTangMulti/Assets/Scripts/Player.cs
@@ -5,22 +5,62 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public int moveSpeed = 10;
-    void Start()
+
+    private Animator animator;
+
+    private Vector3 movePos;
+    
+    private void Start()
     {
-        
+        Initialize();
     }
 
+    /// <summary>
+    /// 플레이어 캐릭터 초기화
+    /// </summary>
+    private void Initialize()
+    {
+        animator = GetComponent<Animator>();
+        
+        movePos = new Vector3();
+    }
+
+    /// <summary>
+    /// 플레이어 캐릭터 이동
+    /// </summary>
     private void Move()
     {
-        var movePos = Vector3.zero;
+        movePos = Vector3.zero;
         
-        movePos.x += Input.GetAxisRaw("Horizontal");
-        movePos.y += Input.GetAxisRaw("Vertical");
+        // 방향 입력 받기
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
-        transform.position += movePos * moveSpeed * Time.deltaTime;
+        // 이동 벡터 계산
+        movePos.x = horizontalInput;
+        movePos.y = verticalInput;
+
+        // 이동 벡터가 0이 아닌 경우에만 방향을 바라보도록 설정
+        if (movePos != Vector3.zero)
+        {
+            // 현재 위치에서 목표 방향을 바라보도록 회전
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, -movePos.normalized);
+        }
+
+        // 이동
+        transform.position += (movePos * moveSpeed * Time.deltaTime);
+    }
+    
+    /// <summary>
+    /// 플레이어 캐릭터 애니메이션
+    /// </summary>
+    /// <param name="isVertical">위 아래 이동 수치</param>
+    private void SetAnimation(float isVertical)
+    {
+        animator.SetFloat("verticalValue", isVertical);
     }
 
-    void Update()
+    private void Update()
     {
         Move();
     }
