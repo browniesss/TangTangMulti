ebe463894fd67cc04aebaeb402cd269173e0530e
Browniesss <abcdkl45678@gmail.com>
2023-09-22 23:43:59 +0900

    Global 스크립트 업로드

---
+++ TangTangMulti/Assets/Scripts/Utils/Global.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Global 클래스는 전역으로 사용되는 String, Const 상수값을 저장하는 클래스
+/// </summary>
+public class Global
+{
+    // EX) 아래와 같은 식으로 구현
+    //public const string INTRO_MSG = "안녕하세요";
+}
